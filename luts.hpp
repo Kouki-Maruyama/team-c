956cb9b7d25e79b9508f71594a4391645a28878c
ishikawa-rintaro <141391467+ishikawa-rintaro@users.noreply.github.com>
2023-09-08 12:04:56 +0900

    Add files via upload

--- luts.hpp
+++ luts.hpp
@@ -2,11 +2,10 @@
 #define STAFFSTRUCT_H_
 // ============================↓↓↓編集可能↓↓↓========================== //
 typedef struct GLCM{
+    int num=0;
     int glcm[256][256]=0;
 }GLCM;
 
-typedef struct COUNT{
-    int count;
-}COUNT;
+
 // ============================↑↑↑編集可能↑↑↑========================== //
 #endif
