ea89bd9588653fa6a57db6ff1920794a3d01d466
KotaUsami203 <141391377+KotaUsami203@users.noreply.github.com>
2023-09-08 15:27:38 +0900

    Update mabiki.cpp

--- mabiki.cpp
+++ mabiki.cpp
@@ -1,8 +1,10 @@
 //候補点を等価間隔で求めるプログラム
 
-int reference_x[50];    //絞った際のx座標の配列
-int reference_y[50];    //絞った際のy座標の配列
+int reference_x[C_SIZE/2];    //絞った際のx座標の配列
+int reference_y[C_SIZE/2];    //絞った際のy座標の配列
+//※注意！！C_SIZEが偶数であることを確認しておくこと（奇数の場合小数点の問題が発生）
 
-for( i = 0; i < ; i++ ){
-    reference_x[i]=refrence
+int count = 0;
+for( i = 0; i < C_SIZE-2; i++ ){
+    reference_x[i]=
 } 
