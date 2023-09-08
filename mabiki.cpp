bab480bd542091a6e0f9a2ced721ccc97b959dfe
KotaUsami203 <141391377+KotaUsami203@users.noreply.github.com>
2023-09-08 15:29:47 +0900

    Update mabiki.cpp

--- mabiki.cpp
+++ mabiki.cpp
@@ -5,6 +5,8 @@ int reference_y[C_SIZE/2];    //絞った際のy座標の配列
 //※注意！！C_SIZEが偶数であることを確認しておくこと（奇数の場合小数点の問題が発生）
 
 int count = 0;
-for( i = 0; i < C_SIZE-2; i++ ){
-    reference_x[i]=
+for( i = 0; i < C_SIZE-2; i+=2 ){
+    reference_x[count] = point_x[i];
+    reference_y[count] = point_y[i];
+    count += 1;
 } 
