ab4eade7f220d8bfb19bbab99b727592de1b5270
Kouki Maruyama <140692750+Kouki-Maruyama@users.noreply.github.com>
2023-09-11 11:19:24 +0900

    Before modify SSDA

--- luts.hpp
+++ luts.hpp
@@ -1,14 +1,17 @@
-#ifndef LUT_H_
-#define LUT_H_
+#ifndef LUTS_H_
+#define LUTS_H_
+
 // ============================↓↓↓編集可能↓↓↓========================== //
 
-#define COM_SIZE 256              // 同時生起行列のサイズ
-#define REFERENCE_SIZE 50         // TMの参照画素数
+#define COM_SIZE 256                     // 同時生起行列のサイズ
+#define REFERENCE_SIZE 100                // TMの参照画素数
 
-int global_count = 0;             // カウンター
-int com[COM_SIZE][COM_SIZE];      // 同時生起行列を保存
-int reference_x[REFERENCE_SIZE];  // 参照画素の座標を保存
-int reference_y[REFERENCE_SIZE];  // 参照画素の座標を保存
+extern int global_count;                 // カウンター
+extern int pattern;                      // パターン判別用
+// extern int REFERENCE_SIZE;               // TMの参照画素数
+extern int com[COM_SIZE][COM_SIZE];      // 同時生起行列を保存
+extern int reference_x[REFERENCE_SIZE];  // 参照画素の座標を保存
+extern int reference_y[REFERENCE_SIZE];  // 参照画素の座標を保存
 
 // 発生頻度の低い画素を保存する構造体
 typedef struct COM_POINT{
