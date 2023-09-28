dece0188d0ad3508aeb7084ea05409cd31b2c451
Kouki Maruyama <140692750+Kouki-Maruyama@users.noreply.github.com>
2023-09-28 14:15:16 +0900

    modify time ( all pattern is under 0.7ms )

--- luts.hpp
+++ luts.hpp
@@ -2,22 +2,22 @@
 #define LUTS_H_
 
 // ============================↓↓↓編集可能↓↓↓========================== //
+
 #define COM_SIZE 256              // 同時生起行列のサイズ
-#define SAVE_SIZE 100             // 保存サイズ
+#define SAVE_SIZE 50              // 保存サイズ 
+#define INPUT_CENTER_X 320        // 入力画像の中心位置
+#define INPUT_CENTER_Y 240        // 入力画像の中心位置
+
 
 int global_count;                 // カウンター
 int pattern;                      // パターン判別用
 int reference_size;               // TMの参照画素数
 int background_pixel;             // 背景画素
+int found_x, found_y;             // スタート位置（パターン2）
 int com[COM_SIZE][COM_SIZE];      // 同時生起行列を保存
 int reference_x[SAVE_SIZE];       // 参照画素の座標を保存
 int reference_y[SAVE_SIZE];       // 参照画素の座標を保存
 
-// 発生頻度の低い画素を保存する構造体
-typedef struct COM_POINT{
-    int p;                        // 始点画素
-    int q;                        // 終点画素
-}COM_POINT;
 // ============================↑↑↑編集可能↑↑↑========================== //
 
 #endif
--- matching.hpp
+++ matching.hpp
@@ -4,9 +4,9 @@
 // ============================↓↓↓編集可能↓↓↓========================== //
 
 Point matching(unsigned char input[CHANNEL][INPUT_SIZE_H][INPUT_SIZE_W], unsigned char temp[CHANNEL][TMP_SIZE_H][TMP_SIZE_W]);
-Point RSSDA_1(unsigned char input_g[INPUT_SIZE_H][INPUT_SIZE_W], unsigned char temp_g[TMP_SIZE_H][TMP_SIZE_W]);
-Point RSSDA_2(unsigned char input_g[INPUT_SIZE_H][INPUT_SIZE_W], unsigned char temp_g[TMP_SIZE_H][TMP_SIZE_W]);
-Point RSSDA_3(unsigned char input_g[INPUT_SIZE_H][INPUT_SIZE_W], unsigned char temp_g[TMP_SIZE_H][TMP_SIZE_W]);
+Point RSSDA_1(unsigned char input[CHANNEL][INPUT_SIZE_H][INPUT_SIZE_W], unsigned char temp[CHANNEL][TMP_SIZE_H][TMP_SIZE_W]);
+Point RSSDA_2(unsigned char input[CHANNEL][INPUT_SIZE_H][INPUT_SIZE_W], unsigned char temp[CHANNEL][TMP_SIZE_H][TMP_SIZE_W]);
+Point RSSDA_3(unsigned char input[CHANNEL][INPUT_SIZE_H][INPUT_SIZE_W], unsigned char temp[CHANNEL][TMP_SIZE_H][TMP_SIZE_W]);
 
 // ============================↑↑↑編集可能↑↑↑========================== //
 
